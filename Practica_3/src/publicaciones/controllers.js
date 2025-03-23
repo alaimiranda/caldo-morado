0b1ac49123d245a4f8207fb8ddb5f58914d4b961
Catalina F. Mora <catalinafullanamora@gmail.com>
2025-03-23 19:34:15 +0100

    estructura perfil entera

--- Practica_3/src/publicaciones/controllers.js
+++ Practica_3/src/publicaciones/controllers.js
@@ -4,15 +4,27 @@ import { Publicacion } from './Publicacion.js';
 export function publish(req, res) {
     
     body('titulo').escape();
+    body('colab1').escape();
+    body('colab2').escape();
+    body('colab3').escape();
+    body('colab4').escape();
+    body('colab5').escape();
+
   
     const titulo = req.body.titulo.trim();
-
+    const colab1 = req.body.colab1.trim();
+    const colab2 = req.body.colab2.trim();
+    const colab3 = req.body.colab3.trim();
+    const colab4 = req.body.colab4.trim(); 
+    const colab5 = req.body.colab5.trim();
     
     try { 
         let date = new Date();
         let dateToString = date.getDate().toString() + "/" + date.getMonth().toString() + "/" + date.getFullYear().toString() + " " + date.getHours().toString() + ":" + date.getMinutes().toString() + ":" + date.getSeconds().toString();
 
-        const publicacion = new Publicacion(titulo, "Ruben", "Alai", "Cata", "Gallo", "Pepe", dateToString);
+        //const publicacion = new Publicacion(titulo, "Ruben", "Alai", "Cata", "Gallo", "Pepe", dateToString);
+        const publicacion = new Publicacion(titulo, colab1, colab2, colab3, colab4, colab5, dateToString);
+
         publicacion.persist();  
 
         return res.render('pagina', {
