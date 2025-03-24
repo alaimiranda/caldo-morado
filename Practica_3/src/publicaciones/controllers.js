33eaf845f5a57698491760c384bb12835424d7be
Catalina F. Mora <catalinafullanamora@gmail.com>
2025-03-24 14:32:08 +0100

    cambios en el select

--- Practica_3/src/publicaciones/controllers.js
+++ Practica_3/src/publicaciones/controllers.js
@@ -1,29 +1,31 @@
 import { body } from 'express-validator';
 import { Publicacion } from './Publicacion.js';
+//import { colaboradores } from '../../static/js/select.js';
+
 
 export function publish(req, res) {
     
     body('titulo').escape();
-    body('colab1').escape();
-    body('colab2').escape();
-    body('colab3').escape();
-    body('colab4').escape();
-    body('colab5').escape();
-
-  
     const titulo = req.body.titulo.trim();
-    const colab1 = req.body.colab1.trim();
-    const colab2 = req.body.colab2.trim();
-    const colab3 = req.body.colab3.trim();
-    const colab4 = req.body.colab4.trim(); 
-    const colab5 = req.body.colab5.trim();
     
     try { 
         let date = new Date();
         let dateToString = date.getDate().toString() + "/" + date.getMonth().toString() + "/" + date.getFullYear().toString() + " " + date.getHours().toString() + ":" + date.getMinutes().toString() + ":" + date.getSeconds().toString();
 
-        //const publicacion = new Publicacion(titulo, "Ruben", "Alai", "Cata", "Gallo", "Pepe", dateToString);
+
+        /*
+        let colab1 = colaboradores[0] || null;
+        let colab2 = colaboradores[1] || null;
+        let colab3 = colaboradores[2] || null;
+        let colab4 = colaboradores[3] || null;
+        let colab5 = colaboradores[4] || null;
+
+        console.log(colaboradores);
         const publicacion = new Publicacion(titulo, colab1, colab2, colab3, colab4, colab5, dateToString);
+        colaboradores = [];
+        */
+
+        const publicacion = new Publicacion(titulo, "Ruben", "Alai", "Cata", "Gallo", "Pepe", dateToString);
 
         publicacion.persist();  
 
--- Practica_3/static/js/select.js
+++ Practica_3/static/js/select.js
@@ -1,30 +1,36 @@
-document.getElementById("add-colab").addEventListener("click", function() {
-    const select = document.getElementById("colab-select");
-    const collaboratorName = select.options[select.selectedIndex].text;
-    document.getElementById("collaborator-name").textContent = collaboratorName;
-});
+export let colaboradores = [];
+
+
 
 document.getElementById('add-colab').addEventListener('click', function() {
     var select = document.getElementById('colab-select');
     var selectedValue = select.value;
     if (selectedValue !== "") {
-        // nuevo elemento
-        var li = document.createElement('li');
-        
-        // nombre
-        var nameSpan = document.createElement('span');
-        nameSpan.textContent = selectedValue;
-        li.appendChild(nameSpan);
-    
-        // eliminar
-        var deleteSpan = document.createElement('span');
-        deleteSpan.textContent = "×";
-        deleteSpan.addEventListener('click', function() {
-            li.remove();
-        });
-        li.appendChild(deleteSpan);
-        
-        document.getElementById('colab-list').appendChild(li);
+        if(!colaboradores.includes(selectedValue)){
+            // nuevo elemento
+            var li = document.createElement('li');
+            
+            // nombre
+            var nameSpan = document.createElement('span');
+            nameSpan.textContent = selectedValue;
+            li.appendChild(nameSpan);
+
+            // eliminar
+            var deleteSpan = document.createElement('span');
+            deleteSpan.textContent = "×";
+
+            deleteSpan.addEventListener('click', function() {
+                li.remove();
+                //eliminar del array
+                colaboradores = colaboradores.filter(item => item !== selectedValue);
+            });
+
+            li.appendChild(deleteSpan);
+            colaboradores[colaboradores.length] = selectedValue;
+            document.getElementById('colab-list').appendChild(li);
+        } else{
+            alert("El colaborador ya ha sido añadido.");
+        }
     } else {
         alert("Por favor, selecciona un colaborador.");
     }
