db866d80c8e08d224d11687c617f02ee146f24e3
rubennmatey <rubenmatey14@gmail.com>
2025-05-12 13:21:48 +0200

    Buscador de amigos para crear chat funcional (falta importar amigos)

--- Practica_4/static/js/busquedaAmigos.js
+++ Practica_4/static/js/busquedaAmigos.js
@@ -1,4 +1,6 @@
-let amigos = []; // Falta importar la lista de seguidores
+
+
+let amigos = ["Ruben", "Diego", "Diana", "Alai", "Alba", "Gallo", "Garcia"]; // TODO: Falta importar la lista de seguidores
 
 function buscar(){
     let query = document.getElementById('chat-buscar').value;
@@ -6,23 +8,29 @@ function buscar(){
         return;
     }
     let result = [];
-
     for(let i = 0; i < amigos.length; i++){
         if(amigos[i].toLowerCase().includes(query.toLowerCase())){
             result.push(amigos[i]);
         }
     }
-
-    document.getElementById('results').innerHTML = "";
+    document.getElementById('chat-results').innerHTML = "";
     if(result.length > 0){
-        for(let i; i < result.length; i++){
+        for(let i = 0; i < result.length; i++){
             let li = document.createElement('li');
             li.textContent = result[i];
-            document.getElementById('results').appendChild(li);
+             li.addEventListener('click', function() {
+                seleccionarUsuario(result[i]);
+            });
+            document.getElementById('chat-results').appendChild(li);
         }
     }else{
         let li = document.createElement('li');
         li.textContent = "No se encontraron amigos con nombre: " + query;
-        document.getElementById('results').appendChild(li);
+        document.getElementById('chat-results').appendChild(li);
     }
+}
+
+function seleccionarUsuario(usuario){
+    document.getElementById('chat-redirect-form').action = `/chat/${usuario}`;
+    document.getElementById('chat-redirect-form').submit();
 }
